a233c6a2de5e2d7f95d36cd677d176525f0fe86d
cuong <cuong@cuong.cuong>
2026-06-27 00:43:02 -0400

    add edge detection

--- c1_openCV/main.cpp
+++ c1_openCV/main.cpp
@@ -9,6 +9,8 @@ int main() {
     }
 
     cv::Mat frame;
+    cv::Mat blurred_frame;
+    cv::Mat edge_frame;
     std::cout << "Press 'q' to exit the video stream." << std::endl;
     while(true) {
         cap >> frame;
@@ -17,8 +19,15 @@ int main() {
             std::cout << "Error capturing frame" << std::endl;
             break;
         }
+        // Apply Gaussian blur to the frame
+        cv::GaussianBlur(frame, blurred_frame, cv::Size(7, 7), 0);
 
-        cv::imshow("Video Stream1", frame);
+        // Apply Canny edge detection
+        cv::Canny(blurred_frame, edge_frame, 50, 150);
+
+        //cv::imshow("Video Stream1", frame);
+        //cv::imshow("Video Stream2", blurred_frame);
+        cv::imshow("Video Stream3", edge_frame);
 
         char key = (char)cv::waitKey(10);
         if(key == 'q' || key == 27) {
